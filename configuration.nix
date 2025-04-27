db8b138e510ce557f6bc0fd073719f70ed756c38
LuckySteve162 <johnlacy162@gmail.com>
2025-04-26 22:40:39 -0500

    Way too many commits, prob gott clean up

--- configuration.nix
+++ configuration.nix
@@ -35,7 +35,7 @@
 
   # Wayland environment
   programs.hyprland.enable = true;
-  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
   services.displayManager.defaultSession = "hyprland";
 
   environment.sessionVariables = {
