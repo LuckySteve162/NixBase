4aa5b2099ca8ad3442f55822b58306efac916301
LuckySteve162 <johnlacy162@gmail.com>
2025-05-04 23:00:12 -0500

    fix again

--- configuration.nix
+++ configuration.nix
@@ -74,7 +74,6 @@
 
   # Remote desktop services
   services.sunshine.enable = true;
-  systemd.user.services.sunshine.enable = true;
 
   # System-wide packages
   environment.systemPackages = with pkgs; [
